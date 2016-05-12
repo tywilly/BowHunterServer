b7193904f3980fe53b06b6a099e27a91b00f3f91
Tyler Wilcox <tywilly300@gmail.com>
2016-05-12 00:54:26 -0400

    Finished merge

--- src/com/tywilly/bowhunter/entity/player/Player.java
+++ src/com/tywilly/bowhunter/entity/player/Player.java
@@ -1,9 +1,13 @@
 package com.tywilly.bowhunter.entity.player;
 
 import com.tywilly.bowhunter.entity.Entity;
+
 import com.tywilly.bowhunter.net.ClientConnection;
+
 import com.tywilly.bowhunter.net.Server;
+
 import com.tywilly.bowhunter.net.protocol.packets.MovePacket;
+
 import com.tywilly.bowhunter.net.protocol.packets.SetHealthPacket;
 
 public class Player extends Entity {
@@ -13,60 +17,117 @@ public class Player extends Entity {
 	private int health = 100;
 
 	private int exp = 0;
-	
-	public Player() {
-		
+
+	private ClientConnection con;
+
+	private long tick = 0;
+
+	public Player(ClientConnection con) {
+
+		this.con = con;
+
 	}
 
 	public String getUsername() {
+
 		return username;
+
 	}
 
 	public void setUsername(String userame) {
+
 		this.username = userame;
+
 	}
 
 	public void setHealth(int health) {
+
 		this.health = health;
 
 		if (this.health > 0) {
-			Server.getClientConnectionByUsername(getUsername()).sendPacket(
-					new SetHealthPacket(getUUID(), getHealth()));
+
+			Server.getClientConnectionByUsername(getUsername()).sendPacket(new SetHealthPacket(getUUID(), getHealth()));
+
 		} else {
+
 			// Die
+
 			onDeath();
+
 		}
-	}
-	
-	public int getHealth() {
-		return health;
-	}
-	
-	private void onDeath(){
-		
-		for(int i=0;i<Server.clients.size();i++){
-			ClientConnection cli = Server.clients.get(i);
-			
-			cli.sendPacket(new MovePacket(getUUID(), 200 + " " + 200));
-		}
-		
-		this.setHealth(100);
-		this.setExp(0);
-		
+
 	}
 
-	public void setExp(int exp){
+	public int getHealth() {
+
+		return health;
+
+	}
+
+	private void onDeath() {
+
+		for (int i = 0; i < Server.clients.size(); i++) {
+
+			ClientConnection cli = Server.clients.get(i);
+
+			cli.sendPacket(new MovePacket(getUUID(), 200 + " " + 200));
+
+		}
+
+		this.setHealth(100);
+
+		this.setExp(0);
+
+	}
+
+	public void setExp(int exp) {
+
 		this.exp = exp;
+
 	}
-	
-	public int getExp(){
+
+	public int getExp() {
+
 		return exp;
+
 	}
-	
+
 	@Override
+
 	public void update() {
+
 		// TODO Auto-generated method stub
-		
+
+		if (tick >= Long.MAX_VALUE) {
+
+			tick = -1;
+
+		}
+
+		if (System.currentTimeMillis() - con.getLastHeartBeatTime() >= 5000 || con.getLastHeartBeatTime() <= 0) {
+
+			if (!con.isHeartBeatSent()) {
+
+				con.sendHeartBeat();
+
+			} else {
+
+				con.disconnect("Client Timedout!");
+
+			}
+
+		} else if (System.currentTimeMillis() - con.getLastHeartBeatTime() >= 4000) {
+
+			if (con.isHeartBeatSent()) {
+
+				con.disconnect("Client Timedout!");
+
+			}
+
+		}
+
+		tick++;
+
 	}
 
 }
